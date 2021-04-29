5f3e0b1df91d7e7b32cc4e4123f2f7edfcccc79b
bruhmoment6420 <admimterminal2000@gmail.com>
2021-04-29 08:46:49 +0000

    made the settings screen

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import Settings from '../stacks/Settings'
+import About from '../stacks/About'
 
 const Stack = createStackNavigator();
 
@@ -9,6 +10,7 @@ export default function NewspaperScreen() {
   return (
     <Stack.Navigator>
       <Stack.Screen name='Settings' component={Settings}/>
+      <Stack.Screen name='About' component={About}/>
     </Stack.Navigator>
   )
 }
--- stacks/Settings.js
+++ stacks/Settings.js
@@ -1,10 +1,58 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Image, StyleSheet } from 'react-native';
+import { List, Divider, Switch } from 'react-native-paper'
 
-export default function Settings() {
+export default function Settings({ navigation }) {
+  const [cachingIsOn, setCachingIsOn] = useState(false)
+  const [trackingIsOn, setTrackingIsOn] = useState(false)
+  const [quickLoadingIsOn, setquickLoadingIsOn] = useState(false)
+
   return (
     <View>
-      <Text>Settings</Text>
+      <View style={styles.imageContainer}>
+        <Image style={{ height: 200, width: 200 }} source={require('../assets/icon.png')} />
+      </View>
+      <View style={styles.settingsContainer}>
+        <List.Item
+          title="Web View Chaching"
+          description="Turn on caching"
+          left={(props) => <List.Icon {...props} icon="cached"/>}
+          right={() => <Switch value={cachingIsOn} onValueChange={() => setCachingIsOn(true)} />} 
+        />
+        <Divider/>
+        <List.Item
+          title="Private Mode"
+          description="Blocks Trackers"
+          left={(props) => <List.Icon {...props} icon="lock"/>}
+          right={() => <Switch value={trackingIsOn} onValueChange={() => setTrackingIsOn(true)} />} 
+        />
+        <Divider/>
+        <List.Item
+          title="Quick Load"
+          description="Activate Quick Loading"
+          left={(props) => <List.Icon {...props} icon="flash"/>}
+          right={() => <Switch value={quickLoadingIsOn} onValueChange={() => setquickLoadingIsOn(true)} />} 
+        />
+        <Divider/>
+        <List.Item
+          title="About"
+          description="About the app and developer"
+          left={(props) => <List.Icon {...props} icon="account"/>}
+          onPress={() => navigation.navigate('About')}
+        />
+      </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  imageContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 12,
+  },
+  settingsContainer: {
+    marginTop: 12,
+    paddding: 8
+  }
+})
