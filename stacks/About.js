9e213755befb27c3edc02b7d28c077dbd5e80f3d
bruhmoment6420 <admimterminal2000@gmail.com>
2021-05-04 16:05:35 +0000

    added the about screen

--- stacks/About.js
+++ stacks/About.js
@@ -1,10 +1,32 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Linking, StyleSheet } from 'react-native'
+import { Divider } from 'react-native-paper'
 
 export default function About() {
   return (
-    <View>
-      <Text>About Screen</Text>
+    <View style={styles.container}>
+      <Divider/>
+      <Text style={styles.textStyle}>Version: 2.0</Text>
+      <Divider/>
+      <Text style={styles.textStyle}>Build: 34</Text>
+      <Divider/>
+      <Text style={styles.textStyle}>Repo: https://github.com/tahlilma/Newserily</Text>
+      <Divider/>
+      <Text style={styles.textStyle}>Made By: Tahlil</Text>
+      <Divider/>
+      <Text style={styles.textStyle}>Libraries Used: React Native Paper, React Navigation</Text>
+      <Divider/>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 12,
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  textStyle: {
+    fontSize: 16
+  }
+})
