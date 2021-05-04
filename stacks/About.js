a8d09efbdf44d00a8c55747ee0990d53a53b2a99
bruhmoment6420 <admimterminal2000@gmail.com>
2021-05-04 16:06:44 +0000

    did some refactoring

--- stacks/About.js
+++ stacks/About.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text, Linking, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import { Divider } from 'react-native-paper'
 
 export default function About() {
